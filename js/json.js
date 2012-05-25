6d6c6a4e2f8994e2387bdae580526ce774b57238
MsCheryl <mscheryl@fullsail.edu>
2012-05-24 21:09:42 -0400

    Uploaded project Git

---
+++ js/json.js
@@ -0,0 +1,80 @@
+//Cheryl Ferguson
+//VFW 1205
+//Project 4
+
+
+var json = {
+	"member1": {
+		"greetInfo": ["Select Appropriate Greeting:", "Miss"],
+		"fname": ["First Name:", "Susie"],
+		"lname": ["Last Name:", "Williams"],
+		"username": ["Username:", "Susiepoo"],
+		"pw": ["Desired Password:","password"],
+		"email": ["Email:", "susiepoo@yourmail.com"],
+		"pn": ["Phone Number:", "123-456-7890"],
+		"gender": ["Gender:", "Female"],
+		"affirm": ["Affirm:", "Yes"],
+		"ease":["Ease of Use:", "7"],
+		"date": ["Date:", "2012-1-1"],
+		"notes": ["Notes:", "Usher Ministry"]
+	},
+	"member2": {
+		"greetInfo": ["Select Appropriate Greeting:", "Ms"],
+		"fname": ["First Name:", "Becky"],
+		"lname": ["Last Name:", "Williams"],
+		"username": ["Username:", "Beck"],
+		"pw": ["Desired Password:","car1"],
+		"email": ["Email:", "beckbeck@mail.com"],
+		"pn": ["Phone Number:", "202-456-7890"],
+		"gender": ["Gender:", "Female"],
+		"affirm": ["Affirm:", "Yes"],
+		"ease":["Ease of Use:", "9"],
+		"date": ["Date:", "2012-2-12"],
+		"notes": ["Notes:", "Music Ministry"]
+	},
+	"member3": {
+		"greetInfo": ["Select Appropriate Greeting:", "Mrs"],
+		"fname": ["First Name:", "Sue"],
+		"lname": ["Last Name:", "Turner"],
+		"username": ["Username:", "TurnSue"],
+		"pw": ["Desired Password:","abc123"],
+		"email": ["Email:", "trunsue@rmail.com"],
+		"pn": ["Phone Number:", "231-659-0000"],
+		"gender": ["Gender:", "Female"],
+		"affirm": ["Affirm:", "Yes"],
+		"ease":["Ease of Use:", "9"],
+		"date": ["Date:", "2010-4-1"],
+		"notes": ["Notes:", "Prison Ministry"]
+	},
+	"member4": {
+		"greetInfo": ["Select Appropriate Greeting:", "Master"],
+		"fname": ["First Name:", "Marquis"],
+		"lname": ["Last Name:", "Williams"],
+		"username": ["Username:", "LoKi"],
+		"pw": ["Desired Password:","outlaw1"],
+		"email": ["Email:", "susiepoo@yourmail.com"],
+		"pn": ["Phone Number:", "123-456-7890"],
+		"gender": ["Gender:", "Female"],
+		"affirm": ["Affirm:", "Yes"],
+		"ease":["Ease of Use:", "7"],
+		"date": ["Date:", "2012-1-1"],
+		"notes": ["Notes:", "Usher Ministry"]
+	},
+	"member5": {
+		"greetInfo": ["Select Appropriate Greeting:", "Mr"],
+		"fname": ["First Name:", "Johnny"],
+		"lname": ["Last Name:", "Walker"],
+		"username": ["Username:", "BigBoog"],
+		"pw": ["Desired Password:","augusta"],
+		"email": ["Email:", "susiepoo@yourmail.com"],
+		"pn": ["Phone Number:", "123-456-7890"],
+		"gender": ["Gender:", "Female"],
+		"affirm": ["Affirm:", "Yes"],
+		"ease":["Ease of Use:", "7"],
+		"date": ["Date:", "2012-1-1"],
+		"notes": ["Notes:", "Usher Ministry"]
+	
+	}
+
+	
+}
